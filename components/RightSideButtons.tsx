9c9504aa4060be5ebf28e248c5b91f5c8480731e
MDR <imdanya946@gmail.com>
2025-07-09 18:33:49 +0300

    add right phone button

--- components/RightSideButtons.tsx
+++ components/RightSideButtons.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { CircleChevronUp } from "lucide-react";
+import { CircleChevronUp, Smartphone } from "lucide-react";
+import Modal from "./Modal";
 export default function RightSideButtons() {
   const [isVisible, setIsVisible] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,6 +25,10 @@ export default function RightSideButtons() {
           isVisible ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
         }`}
       >
+        <Smartphone
+          className="w-14 h-14 md:w-16 md:h-16 text-black p-2 rounded-full hover:scale-105 transition-colors cursor-pointer"
+          onClick={() => setIsModalOpen(true)}
+        />
         <CircleChevronUp
           className="w-14 h-14 md:w-16 md:h-16 text-black p-2 rounded-full hover:scale-105 transition-colors cursor-pointer"
           onClick={() => {
@@ -33,6 +39,7 @@ export default function RightSideButtons() {
           }}
         />
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </>
   );
 }
